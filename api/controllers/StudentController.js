460c3a4f707436c34a125399e13e092b5bf5ff80
aaelgassier <aaelgassier@gmail.com>
2016-07-05 02:32:35 +0200

    Student show action and view

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -24,4 +24,64 @@ module.exports = {
 			res.redirect('/student/show/' + student.id);
 		});
 	},
+	/* render the profie view --> /views/show.ejs*/
+	show: function (req, res, next) {
+		Student.findOne(req.param('id'), function foundStudent (err, student) {
+			if (err) return next(err);
+			if (!student) return next();
+			res.view({
+				student: student
+			});
+		});
+	},
+
+	/* List of all students */
+	index: function (req, res, next) {
+
+		Student.find(function foundStudents (err, students) {
+			if (err) return next(err);
+			/* pass the array down to the /views/index.ejs page */
+			res.view({
+				students: students
+			});
+		});
+	},
+
+	/* render the edit view (e.g. /views/user/edit.ejs)*/
+	edit: function (req, res, next) {
+		/* find the user from the id passed in via params*/
+		Student.findOne(req.param('id'), function foundStudent (err, student){
+			if (err) return next(err);
+			if (!student) return next('Student does not exist.');
+
+			res.view({
+				student: student
+			});
+		});
+	},
+
+	/* Process the info from edit view */
+	update: function (req, res, next) {
+		Student.update(req.param('id'), req.params.all(), function studentUpdated(err) {
+			if (err) {
+				return res.redirect('/student/edit/' + req.param('id'));
+			}
+			res.redirect('/student/show/' + req.param('id'));
+		});
+	},
+
+	destroy: function (req, res, next) {
+		Student.findOne(req.param('id'), function foundStudent (err, student){
+			if (err) return next(err);
+
+			if (!student) return next('Student does not exist.');
+
+			Student.destroy(req.param('id'), function studentDestroyed(err) {
+				if (err) return next(err);
+			});
+
+			res.redirect('/student');
+		});
+	},
+
 };
